5053512ef67798874d21197c027a7f1607dfd995
Erny Salcedo <ernydavid04@gmail.com>
2023-12-16 12:16:47 -0500

    adding h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default async function Home (
   } else {
     return (
       <main className='w-full max-w-7xl h-screen m-auto flex flex-col justify-center items-center gap-3 p-6'>
+        <h1 className='text-5xl text-primary text-center'>Hi!!</h1>
         <h1 className='text-5xl text-primary text-center'>
           {`Welcome back, ${session?.user?.email?.split('@', 1).join(',')}`}
         </h1>
